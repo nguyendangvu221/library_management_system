9539eb45fece29d137e76dec5e6627afa16873e8
nguyendangvu221 <nguyendangvu221@gmail.com>
2023-05-18 01:15:23 +0700

    create folder

---
+++ lib/presentation/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const App(),
+    );
+  }
+}
