34dd578f51cfd080cfd3ac30ba2b7be1ca5bcdda
nguyendangvu221 <nguyendangvu221@gmail.com>
2023-05-24 01:51:33 +0700

    add firebase and add model

---
+++ lib/domain/models/document_model.dart
@@ -0,0 +1,31 @@
+class Document {
+  String? code;
+  String? name;
+  String? author;
+  String? category;
+  String? publisher;
+  String? yearPublication;
+  String? description;
+  int? numberOfPage;
+  String? paperSize;
+  String? reprint;
+  int? numberOfEditions;
+  DateTime? realeaseDate;
+  DateTime? updateDate;
+
+  Document({
+    this.code,
+    this.name,
+    this.author,
+    this.category,
+    this.publisher,
+    this.yearPublication,
+    this.description,
+    this.numberOfPage,
+    this.paperSize,
+    this.reprint,
+    this.numberOfEditions,
+    this.realeaseDate,
+    this.updateDate,
+  })
+}
