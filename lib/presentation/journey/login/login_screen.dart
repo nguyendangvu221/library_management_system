ba910621b68955e47284255ec57aa31ccd0c15da
tanvandiem <tanvandiem@gmail.com>
2023-05-24 00:13:11 +0700

    email
    thanh nhập email

--- lib/presentation/journey/login/login_screen.dart
+++ lib/presentation/journey/login/login_screen.dart
@@ -15,16 +15,45 @@ class LoginScreen extends GetView<LoginController> {
       backgroundColor: AppColors.white,
       body: SafeArea(
         child:Padding(
-          padding: EdgeInsets.all(90),
+          padding: EdgeInsets.all(42),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Hi, Welcome Back!',
-              style: TextStyle(
-                fontSize: 24.sp,
-                fontWeight: FontWeight.w700
 
-              ),),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: Text('Hi, Welcome Back!',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  
+                ),),
+              ),
+              SizedBox(height: 18.h,),
+              Text('Email',textAlign: TextAlign.left,),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(11.r),
+                    borderSide: BorderSide(
+                      color: Color(0x00000000,),
+                      width: 0,
+                    ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(10.r),
+                    borderSide: BorderSide(
+                      color: Color(0x00000000,),
+                      width: 0,
+                    ),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFFFFFFF),
+                    hintText: 'example@gmail.com',
+                ),
+              ),
             ]),
         ),),
       
