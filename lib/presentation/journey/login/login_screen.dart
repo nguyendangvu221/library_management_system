c28615949509282877bf7d5de2c44d69701f6097
nguyendangvu221 <nguyendangvu221@gmail.com>
2023-05-25 19:16:13 +0700

    fix bug

--- lib/presentation/journey/login/login_screen.dart
+++ lib/presentation/journey/login/login_screen.dart
@@ -6,55 +6,67 @@ import 'package:library_management_system/presentation/theme/theme_color.dart';
 
 class LoginScreen extends GetView<LoginController> {
   const LoginScreen({super.key});
-   
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
       body: SafeArea(
-        child:Padding(
-          padding: EdgeInsets.all(42),
+        child: Padding(
+          padding: const EdgeInsets.all(42),
           child: Column(
-
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 alignment: Alignment.bottomCenter,
-                child: Text('Hi, Welcome Back!',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
+                child: const Text(
+                  'Hi, Welcome Back!',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-              SizedBox(height: 18.h,),
-              Text('Email',textAlign: TextAlign.left,),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 18.h,
+              ),
+              const Text(
+                'Email',
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
               TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius:BorderRadius.circular(11.r),
-                    borderSide: BorderSide(
-                      color: Color(0x00000000,),
+                    borderRadius: BorderRadius.circular(11.r),
+                    borderSide: const BorderSide(
+                      color: Color(
+                        0x00000000,
+                      ),
                       width: 0,
                     ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius:BorderRadius.circular(10.r),
-                    borderSide: BorderSide(
-                      color: Color(0x00000000,),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.r),
+                    borderSide: const BorderSide(
+                      color: Color(
+                        0x00000000,
+                      ),
                       width: 0,
                     ),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xFFFFFFFF),
-                    hintText: 'example@gmail.com',
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xFFFFFFFF),
+                  hintText: 'example@gmail.com',
                 ),
               ),
-            ]),
-        ),),
-      
-      
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
