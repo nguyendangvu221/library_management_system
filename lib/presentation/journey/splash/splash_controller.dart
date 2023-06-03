6507e89ebed0d53a1e6ea8b070ea88d8f005634d
tanvandiem <tanvandiem@gmail.com>
2023-06-04 00:38:03 +0700

    update3

--- lib/presentation/journey/splash/splash_controller.dart
+++ lib/presentation/journey/splash/splash_controller.dart
@@ -6,7 +6,7 @@ class SplashController extends GetxController {
   void onReady() {
     super.onReady();
     Future.delayed(const Duration(seconds: 1)).then((value) {
-      Get.offAllNamed(AppRoutes.main);
+      Get.offAllNamed(AppRoutes.login);
     });
   }
 }
