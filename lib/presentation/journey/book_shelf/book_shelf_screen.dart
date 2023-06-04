f54e174ba11d4869aca5f2628a2db23eaf03d7a5
nguyendangvu221 <nguyendangvu221@gmail.com>
2023-06-05 00:17:25 +0700

    fix bug over flow

--- lib/presentation/journey/book_shelf/book_shelf_screen.dart
+++ lib/presentation/journey/book_shelf/book_shelf_screen.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:library_management_system/presentation/journey/book_shelf/book_shelf_controller.dart';
 import 'package:library_management_system/presentation/theme/theme_color.dart';
 import 'package:library_management_system/presentation/theme/theme_text.dart';
-import 'package:library_management_system/presentation/widget/app_touchable.dart';
 
 class BookShelfScreen extends GetView<BookShelfController> {
   const BookShelfScreen({super.key});
@@ -19,11 +18,80 @@ class BookShelfScreen extends GetView<BookShelfController> {
           right: 16.sp,
           top: Get.mediaQuery.padding.top,
         ),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Kệ sách",
               style: ThemeText.heading2.blue800,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Card(
+                      color: AppColors.grey200,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                                height: 160.h,
+                                margin: EdgeInsets.all(7.sp),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.sp),
+                                ),
+                                child: Image(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(
+                                    controller.listDocument[index].image ?? "",
+                                  ),
+                                )),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              margin: EdgeInsets.only(
+                                left: 10.sp,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    controller.listDocument[index].name ?? "",
+                                    style: ThemeText.heading2.s18.blue900,
+                                  ),
+                                  Text(
+                                    "Tác giả: ${controller.listDocument[index].author ?? ""}",
+                                    style: ThemeText.heading4.blue900,
+                                  ),
+                                  Text(
+                                    "Thể loại: ${controller.listDocument[index].category ?? ""}",
+                                    style: ThemeText.bodyMedium.blue900,
+                                  ),
+                                  Text(
+                                    "Số trang: ${controller.listDocument[index].numberOfPage.toString()}",
+                                    style: ThemeText.bodyMedium.blue900,
+                                  ),
+                                  Text(
+                                    "Ngày đăng: ${controller.listDocument[index].releaseDate?.day.toString() ?? DateTime.now()}",
+                                    style: ThemeText.bodyMedium.blue900,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: controller.listDocument.length,
+              ),
             )
           ],
         ),
